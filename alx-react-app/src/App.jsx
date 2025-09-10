e8773e148927bc9a284e6170d3ff4a1c13948c8e
reuben-idan <nobeadonis2@gmail.com>
2025-09-10 11:37:29 +0000

    feat: add Header, MainContent, Footer, and UserProfile components; integrate into App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,12 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
+      <MainContent />
+      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
