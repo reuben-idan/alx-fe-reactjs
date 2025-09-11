391c86050d2ea3c4bbcc6c4f7958c86c39fa567a
reuben-idan <nobeadonis2@gmail.com>
2025-09-11 10:36:34 +0000

    refactor UserInfo.jsx

--- alx-react-app-props/src/UserInfo.jsx
+++ alx-react-app-props/src/UserInfo.jsx
@@ -1,7 +1,7 @@
-import UserProfile from './UserProfile';
+import UserDetails from './UserDetails';
 
 function UserInfo() {
-  return <UserProfile />;
+  return <UserDetails />;
 }
 
 export default UserInfo;
