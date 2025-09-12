72e75275c3cda656f55f2fc157adf6b08235b71a
reuben-idan <nobeadonis2@gmail.com>
2025-09-12 15:08:30 +0000

    Remove duplicate component files from src/ directory

--- src/recipeStore.js
+++ src/recipeStore.js
@@ -1,12 +1,32 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
+  
+  // Add a new recipe
   addRecipe: (newRecipe) => 
-    set((state) => ({ 
-      recipes: [...state.recipes, { ...newRecipe, id: Date.now() }] 
+    set((state) => ({
+      recipes: [...state.recipes, { ...newRecipe, id: Date.now() }]
     })),
+  
+  // Update an existing recipe
+  updateRecipe: (id, updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })),
+  
+  // Delete a recipe
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+  
+  // Set multiple recipes at once (for initialization)
   setRecipes: (recipes) => set({ recipes }),
+  
+  // Alias for deleteRecipe to maintain backward compatibility
   removeRecipe: (id) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== id),
