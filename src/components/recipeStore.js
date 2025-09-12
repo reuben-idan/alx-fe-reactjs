8087548e40d94934fa112bcb043d5f9ce60635b8
reuben-idan <nobeadonis2@gmail.com>
2025-09-12 14:27:10 +0000

    Refactor code snippets

---
+++ src/components/recipeStore.js
@@ -0,0 +1,14 @@
+import { create } from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (newRecipe) => 
+    set((state) => ({ 
+      recipes: [...state.recipes, { ...newRecipe, id: Date.now() }] 
+    })),
+  setRecipes: (recipes) => set({ recipes }),
+  removeRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+}));
