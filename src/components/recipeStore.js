d182261b2d995838677922db952843c31b6b83d9
reuben-idan <nobeadonis2@gmail.com>
2025-09-12 14:43:06 +0000

    update code snippets

--- src/components/recipeStore.js
+++ src/components/recipeStore.js
@@ -1,14 +1,14 @@
-﻿import { create } from 'zustand';
+﻿import { create } from "zustand";
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) => 
-    set((state) => ({ 
-      recipes: [...state.recipes, { ...newRecipe, id: Date.now() }] 
+    set((state) => ({
+      recipes: [...state.recipes, { ...newRecipe, id: Date.now() }]
     })),
   setRecipes: (recipes) => set({ recipes }),
   removeRecipe: (id) =>
     set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+      recipes: state.recipes.filter((recipe) => recipe.id !== id)
     })),
 }));
