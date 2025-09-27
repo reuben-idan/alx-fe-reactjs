2ca44a9b17ad12209d9c64b395cc23a29b516c3d
reuben-idan <nobeadonis2@gmail.com>
2025-09-27 23:11:00 +0000

    feat: add Tailwind CSS integration and UserProfile component

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,12 @@
+import UserProfile from './components/UserProfile'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen p-4">
+      <UserProfile />
+    </div>
+  )
+}
+
+export default App
