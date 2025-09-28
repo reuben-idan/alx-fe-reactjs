71bdd595b3702d6229db1c1e5ea142e10111c1e8
reuben-idan <nobeadonis2@gmail.com>
2025-09-28 07:24:01 +0000

    refactor code snippets

--- github-user-search/src/components/UserProfile.jsx
+++ github-user-search/src/components/UserProfile.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 
 const UserProfile = ({ avatarUrl, name, login }) => {
-  return (
-    <div className="flex items-center space-x-4 md:max-w-sm">
-      <img
-        src={avatarUrl}
-        alt={`${name || login}'s avatar`}
-        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36"
-      />
-      <div>
-        {name && <h2 className="text-xl font-bold">{name}</h2>}
-        <p className="text-gray-600">@{login}</p>
-      </div>
-    </div>
-  );
+return ( <div className="flex items-center space-x-4 md:max-w-sm">
+<img
+src={avatarUrl}
+alt={`${name || login}'s avatar`}
+className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36"
+/> <div>
+{name && <h2 className="text-xl font-bold">{name}</h2>} <p className="text-gray-600">@{login}</p> </div> </div>
+);
 };
 
 export default UserProfile;
