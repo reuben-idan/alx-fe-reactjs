43011262d1d4bee5ef2d2f37acfc27bad7f51c44
reuben-idan <nobeadonis2@gmail.com>
2025-09-28 07:44:05 +0000

    refactor code snippets

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
         alt="User"
         className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4 text-center font-semibold">
+      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 hover:text-blue-500 my-4 text-center font-semibold transition-colors duration-200">
         John Doe
       </h1>
       <p className="text-sm sm:text-base text-gray-600 text-center">
